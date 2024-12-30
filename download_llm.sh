62102fa6fa48c210d07ea8ff69bd403a0d9d5725
aleksandr <ugolkov.prog@gmail.com>
2024-12-30 15:56:41 +0200

    add Finnish language

--- download_llm.sh
+++ download_llm.sh
@@ -16,6 +16,12 @@ if [ ! -d llm/vits-piper-ru_RU-irina-medium ]; then
   git clone https://huggingface.co/csukuangfj/vits-piper-ru_RU-irina-medium llm/vits-piper-ru_RU-irina-medium
 fi
 
+if [ ! -d llm/vits-piper-fi_FI-harri-medium ]; then
+  mkdir llm/vits-piper-fi_FI-harri-medium
+  git clone https://huggingface.co/csukuangfj/vits-piper-fi_FI-harri-medium llm/vits-piper-fi_FI-harri-medium
+fi
+
+
 
 # https://huggingface.co/willwade/mms-tts-multilingual-models-onnx
 # https://huggingface.co/csukuangfj?search_models=ru
